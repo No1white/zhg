9c42923db42c5f3f45066f8a7a0354fa0a00a908
lin.shuping <lin.shuping@iwhalecloud.com>
2021-02-01 22:10:21 +0800

    首页商品处理

--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -14,14 +14,22 @@ class index extends Component {
     super(props);
     this.state = {
       quertyParams: {
-        type: '0'
+        category: '0',
+        page:1,
       }
     }
 
   }
   componentDidMount(){
     this.getHotSale();
-    this.getCommodityList();
+    this.getCommodityList(1);
+  }
+  addPage = ()=> {
+     const {quertyParams} = this.state;
+     quertyParams.page = quertyParams.page+1;
+    this.setState({
+      quertyParams
+    })
   }
   getHotSale = () => {
     const {quertyParams} = this.state;
@@ -32,12 +40,13 @@ class index extends Component {
       },
     });
   }
-  getCommodityList = () => {
+  getCommodityList = (page) => {
     const {quertyParams} = this.state;
+    console.log('222');
     this.props.dispatch({
       type: 'home/getCommodityList',
       payload: {
-        ...quertyParams
+        ...quertyParams,
       },
     });
   }
@@ -53,13 +62,16 @@ class index extends Component {
   handleTabClick = (data)=> {
     const {quertyParams} = this.state;
     const query = quertyParams;
-    query.type = data.type;
+    query.category = data.category;
+    query.page = 1;
     this.setState({
       quertyParams: {
         ...query
       }
     })
     this.getHotSale();
+    this.setState
+    this.getCommodityList();
   }
   handleSearchBtn = (word)=> {
     // const {quertyParams} = this.state;
@@ -70,7 +82,6 @@ class index extends Component {
     //     word,
     //   },
     // });
-    console.log(this.props);
     if(word === undefined || word === null) {
       Toast.info('请输入关键词');
       return;
@@ -80,7 +91,7 @@ class index extends Component {
   }
   renderContent = tab => {
     const {hotList,loading,commodityList,hasMore,history} = this.props;
-    console.log(this.props);
+    const {quertyParams} = this.state;
     return (
       (
         <div>
@@ -88,8 +99,10 @@ class index extends Component {
           <CommodityList
             hotList={hotList}
             header = {true}
+            page={quertyParams.page}
             loading={loading}
             history={history}
+            addPage={this.addPage}
             getCommodityList={this.getCommodityList}
             commodityList= {commodityList}
             hasMore ={hasMore}
@@ -102,31 +115,31 @@ class index extends Component {
     const tabs = [
       {
         title: '全部',
-        type: '0',
+        category: '0',
       },
       { title: '服装',
-        type: '1',
+        category: '1',
       },
       { title: '数码',
-        type: '2',
+        category: '2',
       },
       { title: '百货',
-        type: '3',
+        category: '3',
       },
       { title: '配饰',
-        type: '4',
+        category: '4',
       },
       { title: '潮玩',
-        type: '5',
+        category: '5',
       },
       { title: '美妆',
-        type: '6',
+        category: '6',
       },
       { title: '食品',
-        type: '7'
+        category: '7'
       },
       { title: '家居',
-        type: '8'
+        category: '8'
       },
     ];
     const {hotWords} = this.props;
